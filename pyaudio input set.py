08f97549126c591d95cd82ef17fce5883d639092
johnzeng <Jhon.Zeng@outlook.com>
2024-02-26 05:17:11 +0800

    V2 Development Manual: Update: listening module, stop listening module, system module. This version sets pyaudio to run asynchronously with multiple threads to eliminate the plunger phenomenon that occurs when pyaudio detects audio. This is to prevent the program from being unable to shut down and other operations by entering system commands before detecting the melody.

--- pyaudio input set.py
+++ pyaudio input set.py
@@ -1,6 +1,8 @@
+# display
 import pyaudio
+
 p = pyaudio.PyAudio()
-#print(p)
+# print(p)
 device_index = 0
 
 for i in range(p.get_device_count()):
@@ -11,15 +13,27 @@ for i in range(p.get_device_count()):
 
 print('Selected device index: {}'.format(device_index))
 
-{'index': 1, 'structVersion': 2, 'name': 'snd_rpi_i2s_card: simple-card_codec_link snd-soc-dummy-dai-0 (hw:1,0)', 'hostApi': 0, 'maxInputChannels': 2, 'maxOutputChannels': 2, 'defaultLowInputLatency': 0.005804988662131519, 'defaultLowOutputLatency': 0.005804988662131519, 'defaultHighInputLatency': 0.034829931972789115, 'defaultHighOutputLatency': 0.034829931972789115, 'defaultSampleRate': 44100.0}
-Selected device index: {device_index}
+# output exp：
+# {'index': 1, 'structVersion': 2,
+# 'name': 'snd_rpi_i2s_card: simple-card_codec_link snd-soc-dummy-dai-0 (hw:1,0)',
+# 'hostApi': 0, 'maxInputChannels': 2, 'maxOutputChannels': 2,
+# 'defaultLowInputLatency': 0.005804988662131519,
+# 'defaultLowOutputLatency': 0.005804988662131519,
+# 'defaultHighInputLatency': 0.034829931972789115,
+# 'defaultHighOutputLatency': 0.034829931972789115,
+# 'defaultSampleRate': 44100.0}
+# Selected device index: {device_index}
 
+# set
 import pyaudio
+
 p = pyaudio.PyAudio()
+
+
 def record_wav(duration):
     stream = p.open(format=FORMAT,
-            channels=CHANNELS,
-            rate=RATE,
-            input=True,
-            input_device_index=10,	# 指定设备的index值
-            frames_per_buffer=CHUNK)
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    input_device_index=10,  # 指定设备的index值
+                    frames_per_buffer=CHUNK)
